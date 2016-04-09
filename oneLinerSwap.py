cfe3c5b45f5ca0d10918305b837f80704ceea47a
Reinaldo A. Astudillo R <reinaldo@debianhome>
2016-04-09 23:21:42 +0200

    oneLinerSwap alternative

--- oneLinerSwap.py
+++ oneLinerSwap.py
@@ -1,6 +1,13 @@
 a = 2
 b = 4
 print "a = ",a,"b = ",b
-#One liner swap of variable using tuples
-(a,b) = (b,a)
+
+# One liner swap of variable using tuples
+
+(a, b) = (b, a)
 print "a = ",a,"b = ",b
+
+# To improve the readability 
+# you can write the tuple 
+# without parenthesis, as
+a, b = b, a
